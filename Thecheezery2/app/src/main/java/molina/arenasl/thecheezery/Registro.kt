653bc57e9d3d0fe0042017102aa4462b128cc172
Alex Molina <alex_15870@hotmail.com>
2020-02-23 23:52:01 -0700

    Nueva Aplicacion
    Subida de aplicacion completa a git

---
+++ Thecheezery2/app/src/main/java/molina/arenasl/thecheezery/Registro.kt
@@ -0,0 +1,12 @@
+package molina.arenasl.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
